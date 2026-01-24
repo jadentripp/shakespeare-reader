bfcff44d389b6effea16da7148cf22c2f969c170
Jaden Tripp <jadenitripp@gmail.com>
2026-01-23 20:31:09 -0700

    feat(tauri): Configure Qwen TTS as a sidecar

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -661,6 +661,7 @@ async fn get_thread_max_citation_index(
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
